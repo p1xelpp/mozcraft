901fd27d3d3fefda9b600bcf36bc42dc007cbb52
p1xelpp <toaderidk@gmail.com>
2026-05-22 21:55:22 +0200

    Remove unnecessary docstring from get_legacy_args

--- legacy_args.py
+++ legacy_args.py
@@ -1,10 +1,6 @@
 # legacy_args.py
 
 def get_legacy_args(data):
-    """
-    Geeft jvm_args en game_args terug voor oude versies zoals 1.8.9
-    """
-
     # 1.8.9 has no JVM arguments, only game arguments in a single string
     jvm_args = []
 
